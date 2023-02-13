5a99fa2ab6588f23cbf1015b8093fac02249ddf6
Samir <54107548+sam1rrrr@users.noreply.github.com>
2023-02-13 20:55:53 +0300

    Update api.py

--- api.py
+++ api.py
@@ -2,14 +2,16 @@ import requests
 
 class Intercom:
     def __init__(self):
-        self.stream_url = 'https://video14.sputnik.systems/5da8b494-7f6e-45f1-b0f7-8e784b55415a/video1.ts'
+        self.UUID = ''
+        self.JWT_TOKEN = ''
+        self.stream_url = ''
 
 
     def open_door(self):
-        url = 'http://api.mobile.gorod.sputnik.systems/api/mobile/v2/devices/30aeea0e-6fbf-4a28-bcb1-ecbc45e6a8ca/open_door'
+        url = f'http://api.mobile.gorod.sputnik.systems/api/mobile/v2/devices/{UUID}/open_door'
 
         headers = {
-            "jwt": "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0NjY3MzcsInNlc3Npb24iOjQ2OTQwNiwiZXhwaXJhdGlvbiI6MTYzOTUwNTYzN30.Cn9vl4oYH4cBkMqVR2cE4-wtQ4dyXMNvttsk4N8YCsE",
+            "jwt": self.JWT_TOKEN,
             "Host": "api.mobile.gorod.sputnik.systems",
             "Connection": "Keep-Alive",
             "Accept-Encoding": "gzip",
